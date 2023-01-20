0990a957cb58fa0c91d71c60407b2e500f962809
HyunSun <eparksuncafe@gmail.com>
2023-01-20 15:17:08 -0600

    Update jsBoilerPlate.js

--- topics/jsBoilerPlate.js
+++ topics/jsBoilerPlate.js
@@ -1,8 +1,9 @@
 ## Boiler Plates
 
-#### Repeat a string
-```javascript
+### Strings
 
+#### Repeat a String
+```javascript
 //Example:
 let str = "Hello";
 let newStr = str.repeat(3);
@@ -15,3 +16,25 @@ function repeatWord(num, str) {
 }
 repeatWord(2, "Hello"); // "HelloHello"
 ```
+
+### Arrays
+
+#### Generate Random Selection
+
+* To generate random selection from an array <br>
+```javascript
+const arrayChoices = ['choice1', 'choice2', 'choice3' ...etc];
+const randomSelect = arrayChoices[Math.floor(Math.random()*arrayChoices.length)];
+```
+#### Check in Array for Element
+```javascript
+// Declare an array and an element to check
+let myArray = [1, 2, 3, 4, 5];
+let elementToCheck = 3;
+
+// Check if the array includes the element
+if (myArray.includes(elementToCheck)) {
+    console.log(`The element ${elementToCheck} is present in the array.`);
+} else {
+    console.log(`The element ${elementToCheck} is not present in the array.`);
+}
