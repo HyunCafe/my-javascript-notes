6444c6009e5db49e32e23a6b5d12566a6e8745f3
HyunSun <eparksuncafe@gmail.com>
2023-01-20 15:14:27 -0600

    Create jsBoilerPlate.js

---
+++ topics/jsBoilerPlate.js
@@ -0,0 +1,17 @@
+## Boiler Plates
+
+#### Repeat a string
+```javascript
+
+//Example:
+let str = "Hello";
+let newStr = str.repeat(3);
+console.log(newStr); // Output: "HelloHelloHello"
+
+//Example 2:
+function repeatWord(num, str) {
+  let repeatedString = repeatStr(num, str);
+  console.log(repeatedString);
+}
+repeatWord(2, "Hello"); // "HelloHello"
+```
